17863837824a88b5eb385dce8687a7a30ed833c5
Unknown <Nils.Buhmann95@gmail.com>
2016-12-09 00:49:07 +0100

    added more tests and added more DOKU.

--- workspace/shop/src/test/TestArticle.java
+++ workspace/shop/src/test/TestArticle.java
@@ -10,11 +10,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import persistence.Article4Public;
-import persistence.ArticleWrapper4Public;
-import persistence.Cache;
-import persistence.PersistenceException;
-import persistence.Producer4Public;
+import persistence.*;
+
+import java.util.Iterator;
 
 /**
  * Created by A13531 on 05.12.2016.
@@ -75,5 +73,58 @@ public class TestArticle {
         exception.expect(EmptyDefinitionException.class);
         Producer4Public producer = ProducerLst.getTheProducerLst().createProducer("Test");
         ArticleWrapper4Public testArt1 = ComponentManager.getTheComponentManager().newArticle("", new Fraction(5),10, 100, 4, producer);
+        ArticleWrapper4Public testArt2 = ComponentManager.getTheComponentManager().newArticle(" ", new Fraction(5),10, 100, 4, producer);
+    }
+
+    /**
+     * Testfall zum Anlegen zweier Produktgruppen mit derselben Bezeichnung unter einem Hersteller.
+     * @throws Exception
+     */
+    @Test
+    public void DoubleProductGroupNameTest() throws Exception {
+        exception.expect(DoubleDefinitionException.class);
+        Producer4Public producer = ProducerLst.getTheProducerLst().createProducer("Test");
+        String groupName = "TestGrp";
+        ProductGroup4Public testGr1 = ComponentManager.getTheComponentManager().newProductGroup(groupName);
+        ProductGroup4Public testGr2 = ComponentManager.getTheComponentManager().newProductGroup(groupName);
+    }
+
+    /**
+     * Testfall zum Anlegen einer Produktgruppen mit einer leeren Bezeichnung.
+     * @throws Exception
+     */
+    @Test
+    public void EmptyProductGroupNameTest() throws Exception {
+        exception.expect(EmptyDefinitionException.class);
+        Producer4Public producer = ProducerLst.getTheProducerLst().createProducer("Test");
+        ProductGroup4Public testGr1 = ComponentManager.getTheComponentManager().newProductGroup("");
+        ProductGroup4Public testGr2 = ComponentManager.getTheComponentManager().newProductGroup(" ");
+    }
+
+    /**
+     * Testfall zum Ändern einer Artikelbezeichnung.
+     * @throws Exception
+     */
+    @Test
+    public void ChangeArticleNameTest() throws Exception {
+        String newName = "Brot";
+        Producer4Public producer = ProducerLst.getTheProducerLst().createProducer("Test");
+        ArticleWrapper4Public testArt1 = ComponentManager.getTheComponentManager().newArticle("Toast", new Fraction(5),10, 100, 4, producer);
+        Shop.getTheShop().changeArticleName(testArt1, newName);
+        Assert.assertEquals(newName, testArt1.getName());
+    }
+
+    /**
+     * Testfall zum Verschieben eines Artikels von einer Produktgruppe in eine andere.
+     * @throws Exception
+     */
+    @Test
+    public void MoveArticelToOtherProductGroupTest() throws Exception {
+        Producer4Public producer = ProducerLst.getTheProducerLst().createProducer("Test");
+        ArticleWrapper4Public testArt1 = ComponentManager.getTheComponentManager().newArticle("Toast", new Fraction(5),10, 100, 4, producer);
+        ProductGroup4Public testProdGruppe1 = ComponentManager.getTheComponentManager().newProductGroup("ProduktgruppeTest");
+        testArt1.moveTo(testProdGruppe1);
+        assert false;
+        //TODO! Test product group has moved Assert.assertEquals(testProdGruppe1.getName(), ComponentManager.getTheComponentManager().);
     }
 }
